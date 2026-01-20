0272b5bf8e5e917d720a5243d40c169adccf32a9
Dano913 <daniaxve913@gmail.com>
2026-01-20 18:32:18 +0100

    deploy7

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,124 +1,155 @@
-<?php
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎬</text></svg>">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick.css">
-        <link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick-theme.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet">
-        <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-        <link rel="stylesheet" href="{{asset('css/style.css')}}">
-        <title>MovieMind</title>
-
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <div style="width: 60px;">
-                    <img src="{{asset('images/portada.jpg')}}" alt="" width="40px">
-                </div>
-                <a class="navbar-brand" href="#">MovieMind</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Favoritos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">+ Vistos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Estrenos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">TV Directo</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Categorias
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Acción</a></li>
-                                <li><a class="dropdown-item" href="#">Animación</a></li>
-                                <li><a class="dropdown-item" href="#">Aventuras</a></li>
-                                <li><a class="dropdown-item" href="#">Ciencia ficción</a></li>
-                                <li><a class="dropdown-item" href="#">Comedia</a></li>
-                                <li><a class="dropdown-item" href="#">Crimen</a></li>
-                                <li><a class="dropdown-item" href="#">Cristianos</a></li>
-                                <li><a class="dropdown-item" href="#">Documentales</a></li>
-                                <li><a class="dropdown-item" href="#">Drama</a></li>
-                                <li><a class="dropdown-item" href="#">Educativo</a></li>
-                                <li><a class="dropdown-item" href="#">Historia</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                <div class="d-flex">
-                    <button class="btn btn-outline-success" type="submit">Iniciar Sesión</button>
-                </div>
-                </div>
-            </div>
-        </nav>
-        <div class="container">
-            <center>
-                <h1 class="michroma-regular">MovieMind</h1>
-                <button class="btn-facebook btn btn-primary"><i class="bi bi-spotify"></i></button>
-
-                <form class="d-flex sform">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </center>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Plataforma de películas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+    <meta name="color-scheme" content="light dark" />
+    <meta name="theme-color" content="#007bff" media="(prefers-color-scheme: light)" />
+    <meta name="theme-color" content="#1a1a1a" media="(prefers-color-scheme: dark)" />
+    <meta name="title" content="Layout | AdminLTE 4" />
+    <meta name="author" content="ColorlibHQ" />
+    <meta
+      name="description"
+      content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS. Fully accessible with WCAG 2.1 AA compliance."
+    />
+    <meta
+      name="keywords"
+      content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard, accessible admin panel, WCAG compliant"
+    />
+    <meta name="supported-color-schemes" content="light dark" />
+    <link rel="preload" href="{{ asset('dist/css/adminlte.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css"
+      integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q="
+      crossorigin="anonymous"
+      media="print"
+      onload="this.media='all'"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/styles/overlayscrollbars.min.css"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  </head>
+  <body class="layout-fixed sidebar-expand-lg sidebar-open bg-body-tertiary">
+    <div class="app-wrapper">
+      <nav class="app-header navbar navbar-expand bg-body">
+        <div class="container-fluid">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+                <i class="bi bi-list"></i>
+              </a>
+            </li>
+          </ul>
         </div>
-
-        <div class="container">
-            <div class="row">
-                <section class="regular slider">
-                    <div>
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.gHXLbyOVA8Nu4Z44JjVjCgHaFj%3Fpid%3DApi&f=1&ipt=29f4443b79cd92968aea5b7649dbdb3de4e1fe369eb6a58e33811be958eb3508&ipo=images">
-                    </div>
-                    <div>
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.essxtZ4rgK1yApIAjQLdmAHaEK%3Fpid%3DApi&f=1&ipt=25f94cd83b242e264ea272271bdac9701a3e943652c0328edf3ef662962ae981&ipo=images">
-                    </div>
-                    <div>
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.oF4Dy1I0_Rw86gk0LtmPUQHaE7%3Fpid%3DApi&f=1&ipt=d78421a3117268cb8a2f89d2f9b9ee7a40eec792bbe7da75a50ec6107bd8541a&ipo=images">
-                    </div>
-                    <div>
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.gHXLbyOVA8Nu4Z44JjVjCgHaFj%3Fpid%3DApi&f=1&ipt=29f4443b79cd92968aea5b7649dbdb3de4e1fe369eb6a58e33811be958eb3508&ipo=images">
-                    </div>
-                    <div>
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.essxtZ4rgK1yApIAjQLdmAHaEK%3Fpid%3DApi&f=1&ipt=25f94cd83b242e264ea272271bdac9701a3e943652c0328edf3ef662962ae981&ipo=images">
-                    </div>
-                    <div>
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.oF4Dy1I0_Rw86gk0LtmPUQHaE7%3Fpid%3DApi&f=1&ipt=d78421a3117268cb8a2f89d2f9b9ee7a40eec792bbe7da75a50ec6107bd8541a&ipo=images">
-                    </div>
-                </section>
-            </div>
+      </nav>
+      <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+        <div class="sidebar-brand">
+          <a href="../index.html" class="brand-link">
+            <img
+              src="{{asset('dist/assets/img/AdminLTELogo.png')}}"
+              alt="AdminLTE Logo"
+              class="brand-image opacity-75 shadow"
+            />
+            <span class="brand-text fw-light">Movie Mind</span>
+          </a>
         </div>
-
-    <script src="https://kenwheeler.github.io/slick/slick/slick.js" type="text/javascript" charset="utf-8"></script>
-    <script type="text/javascript">
-        $(document).on('ready', function() {
-        
-        $(".regular").slick({
-            dots: true,
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3
-        });
-        });
+        <div class="sidebar-wrapper">
+          <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul
+              class="nav sidebar-menu flex-column"
+              data-lte-toggle="treeview"
+              role="navigation"
+              aria-label="Main navigation"
+              data-accordion="false"
+              id="navigation"
+            >
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon bi bi-speedometer"></i>
+                  <p>
+                    Películas
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="{{url('admin/peliculas')}}" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Listado de Películas</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{url('admin/peliculas/create')}}" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Nueva película</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon bi bi-door-closed"></i>
+                  <p>
+                    Cerrar Sesión
+                  </p>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </aside>
+      <main class="app-main">
+        @yield('content')
+      </main>
+      <footer class="app-footer">
+        &copy; 2025 Movie Mind. Todos los derechos reservados.
+      </footer>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/browser/overlayscrollbars.browser.es6.min.js"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"
+      crossorigin="anonymous"
+    ></script>
+    <script src="{{ asset('dist/js/adminlte.js') }}"></script>
+    <script>
+      const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
+      const Default = {
+        scrollbarTheme: 'os-theme-light',
+        scrollbarAutoHide: 'leave',
+        scrollbarClickScroll: true,
+      };
+      document.addEventListener('DOMContentLoaded', function () {
+        const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+        if (sidebarWrapper && OverlayScrollbarsGlobal?.OverlayScrollbars !== undefined) {
+          OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+            scrollbars: {
+              theme: Default.scrollbarTheme,
+              autoHide: Default.scrollbarAutoHide,
+              clickScroll: Default.scrollbarClickScroll,
+            },
+          });
+        }
+      });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </body>
+    @yield('scripts')
+  </body>
 </html>
